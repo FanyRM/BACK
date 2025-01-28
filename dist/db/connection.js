ed5e01f5df2830f1a324aae7489ac5dc2b8019de
Leosdelrey1111 <hernandezleonel20434@gmail.com>
2025-01-28 07:43:39 -0600

    Correcciones para implementar sesiones

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-/* const sequelize = new Sequelize('mr_macondo', 'root', '123456789', { //Recuerda cambiar contraseña
+const sequelize = new sequelize_1.Sequelize('Mr_macondo', 'root', '123456', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false
-  }); */
-const sequelize = new sequelize_1.Sequelize('Mr_macondo', 'root', 'celeste1234', {
-    dialect: 'mysql',
-    logging: false
 });
+// const sequelize = new Sequelize('Mr_macondo', 'root', '123456', {
+//   dialect: 'mysql',
+//   logging: false
+// });
 exports.default = sequelize;
